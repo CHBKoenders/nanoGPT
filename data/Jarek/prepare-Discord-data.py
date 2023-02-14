9d1c5bdef4e97649c676e74abd369e55752343e2
chbkoenders <kees.koenders@donders.ru.nl>
2023-02-14 22:07:59 +0100

    Functioning

--- data/Jarek/prepare-Discord-data.py
+++ data/Jarek/prepare-Discord-data.py
@@ -7,38 +7,79 @@ from numpy.random import default_rng
 rng = default_rng()
 
 # Access the scraped discord messages
-dir = "./DiscordData/Jarek"
-tot = 0
+dir = "../DiscordData/Jarek"
+
+
+def check_time(curr_time, time, curr_half, half, window):
+    # get hour minutes in integers
+    curr_h, curr_m = curr_time.split(':')
+    h, m = time.split(':')
+    curr_h = int(curr_h)
+    h = int(h)
+    curr_m = int(curr_m)
+    m = int(m)
+
+    # convert to 24h
+    if curr_half == "PM":
+        curr_h += 12
+    if half == "PM":
+        h += 12
+
+    if (h  - curr_h) % 24 < 1:
+        return (h - curr_h) % 60 <= window
+    
+    return False
+
+
+def check_date_and_time(curr_date, date, window):
+    date, time, half = date.split(' ')
+    curr_date, curr_time, curr_half = curr_date.split(' ')
+    if date == curr_date:
+        return check_time(curr_time, time, curr_half, half, window)
+    return False
 
 messages = []
 for fnamne in os.listdir(dir):
     path = os.path.join(dir, fnamne)
+    """
+        TODO: combine messages that are close in time into a single message.
+    """
     df = pandas.read_csv(path)
-    messages.append(df['Content'])
+    df = df[~df['Content'].isnull()]
+    window = 1  # in minutes
+    sample = df.iloc[0]
+    curr_date = 'x x x'
+    curr_msg = ''
+    for i in range(len(df)):
+        sample = df.iloc[i]
+        date = sample['Date']
+        msg = sample['Content']
+        valid = check_date_and_time(curr_date, date, window)
 
-messages = pandas.concat(messages)
-n_tot = len(messages)
-# filter out NaN
-messages = messages[~messages.isnull()]
+        if not valid: messages.append(curr_msg)
 
+        curr_msg = curr_msg + '\n' + msg if valid else msg
+        curr_date = date if not valid else curr_date
+messages = np.array(messages)[1:]
 n = len(messages)
-p = rng.permutation(n)
+
+# p = rng.permutation(n)
+p = np.arange(n)  # not permuting as message order is of importance
+# Maybe to reduce bias we could do mini-batch shuffeling or so?
 frac = 0.9
 
-print(f"From a total of {n_tot} messages, {n_tot - n} have been filtered out\n")
-
-train_data = messages.iloc[p[:int(n * frac)]]
-valid_data = messages.iloc[p[int(n * frac):]]
+train_data = messages[p[:int(n * frac)]]
+valid_data = messages[p[int(n * frac):]]
 print(f"Splitting data into {len(train_data)} training, and {len(valid_data)} "
       "validation messages")
     
 # encode with tiktoken gpt2 bpe
 enc = tiktoken.get_encoding("gpt2")
-eot_token = enc.eot_token
 def encode_messages(messages):
-    ids = []
+    eot_token = enc.eot_token
+    ids = [eot_token] # eot seperates messages, so indicates start as well
     for msg in messages:
-        msg = "\n"+msg
+        msg = "JarekGTP: "+msg # condition on answer-style
         curr_ids = enc.encode_ordinary(msg)
         curr_ids.append(eot_token)
         ids += curr_ids
