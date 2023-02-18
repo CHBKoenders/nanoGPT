a01972849c84528fb21607b631669cb662280d2f
chbkoenders <kees.koenders@donders.ru.nl>
2023-02-18 16:49:43 +0100

    Update

---
+++ config/Jarek_cuda.py
@@ -0,0 +1,25 @@
+out_dir = '../Out/Jarek/'
+eval_interval = 5
+eval_iters = 40
+wandb_log = True # feel free to turn on
+wandb_project = 'JarekGPT'
+
+device = "cuda"
+
+dataset = 'Jarek'
+init_from = 'gpt2-xl'
+# init_from = 'resume'
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = False
+
+# the number of examples per iter:
+# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
+# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
+batch_size = 1
+gradient_accumulation_steps = 32
+max_iters = 210
+
+# finetune at constant LR
+learning_rate = 1.2e-5
+decay_lr = False
