a03e3abeaf75368d07f8d148932e006b4951c866
chbkoenders <kees.koenders@donders.ru.nl>
2023-02-19 18:26:19 +0100

    Update

--- config/Jarek_cuda_large.py
+++ config/Jarek_cuda_large.py
@@ -18,9 +18,9 @@ always_save_checkpoint = False
 # 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
-gradient_accumulation_steps = 32
+gradient_accumulation_steps = 16
 max_iters = 210
 
 # finetune at constant LR
-learning_rate = 1.2e-5
+learning_rate = 1.05e-5
 decay_lr = False
